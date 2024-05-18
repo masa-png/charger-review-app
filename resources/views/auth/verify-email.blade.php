13d33af606ba849b0bc021c4f3cdb90ab8497731
masa-png <adidas.org@icloud.com>
2024-05-18 19:28:36 +0900

    メール認証画面を作成

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,19 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('layouts.app')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+@section('content')
+    <div class="container pt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <h2 class="text-center mb-3">会員登録ありがとうございます！</h2>
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+                <p class="text-center lh-lg mb-5">
+                    現在、仮会員の状態です。ただいま、ご入力頂いたメールアドレス宛に<br>ご本人様確認用のメールをお送りしました。<br>メール本文内のURLをクリックすると本会員登録が完了となります。
+                </p>
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                <div class="text-center">
+                    <a href="{{ url('/') }}" class="btn submit-button w-75 text-white">トップページへ</a>
+                </div>
             </div>
-        </form>
-
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+        </div>
     </div>
-</x-guest-layout>
+@endsection
