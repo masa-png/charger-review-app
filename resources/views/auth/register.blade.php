44a2012253025ef34b4e13c816b92c81651cb333
masa-png <adidas.org@icloud.com>
2024-05-18 19:22:16 +0900

    新規会員登録ページを実装

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,89 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.app')
+@section('content')
+    <div class="container pt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <h2 class="mb-3">新規会員登録</h2>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <hr class="mb-4">
+
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
+
+                    <div class="form-group row mb-3">
+                        <label for="name" class="col-md-5 col-form-label text-md-left fw-medium">ユーザー名<span
+                                class="ms-1 require-input-label"><span
+                                    class="require-input-label-text">必須</span></span></label>
+
+                        <div class="col-md-7">
+                            <input type="text" name="name" id="name"
+                                class="form-control @error('name') is-invalid
+                            @enderror login-input"
+                                value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="田中 太郎">
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>ユーザー名を入力してください</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-3">
+                        <label for="email" class="col-md-5 col-form-label text-md-left fw-medium">メールアドレス<span
+                                class="ms-1 require-input-label"><span
+                                    class="require-input-label-text">必須</span></span></label>
+
+                        <div class="col-md-7">
+                            <input type="email" name="email" id="email"
+                                class="form-control @error('email') is-invalid
+                            @enderror login-input"
+                                value="{{ old('email') }}" required autocomplete="email" placeholder="charger@charger.com">
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>メールアドレスを入力してください</strong>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-3">
+                        <label for="password" class="col-md-5 col-form-label text-md-left fw-medium">パスワード<span
+                                class="ms-1 require-input-label"><span
+                                    class="require-input-label-text">必須</span></span></label>
+
+                        <div class="col-md-7">
+                            <input type="password" name="password" id="password"
+                                class="form-control @error('password') is-invalid
+                            @enderror login-input"
+                                required autocomplete="new-password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-4">
+                        <label for="password-confirm" class="col-md-5 col-form-label text-md-left fw-medium">パスワード(確認用)<span
+                                class="ms-1 require-input-label"><span
+                                    class="require-input-label-text">必須</span></span></label>
+                        <div class="col-md-7">
+                            <input type="password" name="password_confirmation" id="password-confirm"
+                                class="form-control login-input" required autocomplete="new-password">
+                        </div>
+                    </div>
+
+                    <hr class="mb-4">
+
+                    <button type="submit" class="btn submit-button w-100 text-white">
+                        アカウント作成
+                    </button>
+                </form>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
