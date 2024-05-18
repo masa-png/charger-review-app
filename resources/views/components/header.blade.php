9d064b4ab3908c0760ee29a0bfd40cda5c1f6033
masa-png <adidas.org@icloud.com>
2024-05-18 17:05:49 +0900

    ログインしている時の挙動を追加

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,28 +5,43 @@
                 {{-- <img src="{{ asset('img/logo.jpg') }}"> --}}
                 {{ config('app.name', 'Laravel') }}
             </a>
+            @auth
+                <a href="{{ route('logout') }}" class="link-dark"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
+                    @csrf
+                </form>
+            @endauth
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item me-4">
-                        <a href="{{ route('top') }}" class="navlink fw-bold">TOP</a>
-                    </li>
                     @guest
                         <li class="nav-item me-4">
-                            <a href="{{ route('login') }}" class="navlink fw-bold">マイページ</a>
+                            <a href="{{ route('top') }}" class="navlink fw-bold">TOP</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a href="{{ route('login') }}" class="navlink fw-bold">ログイン</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a href="{{ route('top') }}" class="navlink fw-bold">レビュー一覧</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a href="{{ route('top') }}" class="navlink fw-bold">お問い合わせ</a>
                         </li>
                     @else
                         <li class="nav-item me-4">
+                            <a href="{{ route('top') }}" class="navlink fw-bold">TOP</a>
+                        </li>
+                        <li class="nav-item me-4">
                             <a href="{{ route('top') }}" class="navlink fw-bold">マイページ</a>
                         </li>
+                        <li class="nav-item me-4">
+                            <a href="{{ route('top') }}" class="navlink fw-bold">レビュー一覧</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a href="{{ route('top') }}" class="navlink fw-bold">お問い合わせ</a>
+                        </li>
                     @endguest
-
-                    <li class="nav-item me-4">
-                        <a href="{{ route('top') }}" class="navlink fw-bold">レビュー一覧</a>
-                    </li>
-                    <li class="nav-item me-4">
-                        <a href="{{ route('top') }}" class="navlink fw-bold">お問い合わせ</a>
-                    </li>
                 </ul>
             </div>
         </div>
