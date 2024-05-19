6e2d32bb665f11a511ed607799af8dfc30fe228f
masa-png <adidas.org@icloud.com>
2024-05-19 20:27:06 +0900

    マイページのviewを編集

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -1,3 +1,37 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container pt-5">
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-5">
+                <h1>マイページ</h1>
+
+                <div class="container mt-3">
+                    <div class="card text-center py-3 px-3" style="width: 26rem;">
+                        <div>
+                            <img src="" class="card-img-top" alt="プロフィール画像">
+                        </div>
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-5 text-body-secondary">{{ $user->name }}</h6>
+                            <a href="{{ route('mypage.edit') }}" class="btn btn-primary d-block mb-3 py-2">レビューを投稿する</a>
+                            <a href="{{ route('mypage.edit') }}" class="btn btn-secondary d-block mb-3 py-2">会員情報を編集する</a>
+                            <a href="{{ route('logout') }}" class="btn btn-danger d-block m-auto py-2 w-75">ログアウト</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container mt-4">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a href="" class="nav-link active" aria-current="page">投稿した記事</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link active" aria-current="page">記事</a>
+                        </li>
+                    </ul>
+                    <p class="mt-3">投稿した記事はありません</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
