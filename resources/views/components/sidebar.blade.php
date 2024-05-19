0e522228e371b44ee572bace2b8285810042b5cb
masa-png <adidas.org@icloud.com>
2024-05-19 16:47:24 +0900

    サイドバーの追加

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="pt-5">
+    <div class="mb-4">
+        <h2>メーカー一覧</h2>
+        <div class="mb-3">
+            <label class="sidebar-label"><a href="{{ route('reviews') }}"
+                    class="h5 link-dark text-decoration-none">Anker</a></label>
+        </div>
+        <h2>W数一覧</h2>
+        <div class="mb-3">
+            <label class="sidebar-label"><a href="{{ route('reviews') }}"
+                    class="h5 link-dark text-decoration-none">15W〜20W</a></label>
+        </div>
+    </div>
+</div>
