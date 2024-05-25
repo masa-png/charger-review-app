4c3a89ada320c01a13832434a1997c6e1a85932a
masa-png <adidas.org@icloud.com>
2024-05-25 17:19:01 +0900

    DBからメーカー名とW数を取得

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,19 @@
 <div class="pt-5">
     <div class="mb-4">
         <h2>メーカー一覧</h2>
-        <div class="mb-3">
-            <label class="sidebar-label"><a href="{{ route('reviews') }}"
-                    class="h5 link-dark text-decoration-none">Anker</a></label>
-        </div>
+        @foreach ($vendors as $vendor)
+            <div class="mb-3">
+                <label class="sidebar-label"><a href="{{ route('products', ['vendor' => $vendor->id]) }}"
+                        class="h5 link-dark text-decoration-none">{{ $vendor->name }}</a></label>
+            </div>
+        @endforeach
+
         <h2>W数一覧</h2>
-        <div class="mb-3">
-            <label class="sidebar-label"><a href="{{ route('reviews') }}"
-                    class="h5 link-dark text-decoration-none">15W〜20W</a></label>
-        </div>
+        @foreach ($wattages as $wattage)
+            <div class="mb-3">
+                <label class="sidebar-label"><a href="{{ route('products', ['wattage' => $wattage->id]) }}"
+                        class="h5 link-dark text-decoration-none">{{ $wattage->watt }}W</a></label>
+            </div>
+        @endforeach
     </div>
 </div>
