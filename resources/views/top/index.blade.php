fe83316dfae0be011c5033772dba36d52e4c1408
masa-png <adidas.org@icloud.com>
2024-05-25 09:56:58 +0900

    擬似要素に変更

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -10,8 +10,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg12 text-center">
-                    <h1 class="mb-4">USB PDとは？</h1>
-                    <p class="title-border mb-5"></p>
+                    <h1 class="mb-4 sec-title">USB PDとは？</h1>
                 </div>
             </div>
         </div>
@@ -22,7 +21,7 @@
                     <img src="{{ asset('img/USB-PD.png') }}" class="img-fluid">
                 </div>
                 <div class="col-md-6">
-                    <h1 style="color: red;" class="fw-bold mb-3">01</h1>
+                    <h1 style="color: #C60E31;" class="fw-bold mb-3">01</h1>
                     <h2>USB PDとは「USB Power Delivery」の略称！</h2>
                     <p>USB規格を作成している団体によって定められた、Type-Cに<br>
                         対応した給電規格のこと。最大100Wの電力を供給できるよう<br>
@@ -34,7 +33,7 @@
 
             <div class="row mb-5">
                 <div class="col-md-6">
-                    <h1 style="color: red" class="fw-bold mb-3">02</h1>
+                    <h1 style="color: #C60E31" class="fw-bold mb-3">02</h1>
                     <h2>USB PDのメリット！</h2>
                     <p>対応機器を接続するだけでUSB PD規格による急速充電を<br>
                         自動的に行うことが出来る。接続機器側は充電器側の<br>
@@ -51,7 +50,7 @@
                     <img src="{{ asset('img/w数.png') }}" class="img-fluid">
                 </div>
                 <div class="col-md-6">
-                    <h1 style="color: red" class="fw-bold mb-3">03</h1>
+                    <h1 style="color: #C60E31" class="fw-bold mb-3">03</h1>
                     <h2>USB PD対応の充電器を選ぶ際の注意点！</h2>
                     <p>特に消費電力の大きいノートパソコンなどは、機器側が<br>
                         必要とする電力に充電器側が対応しているか「W数を」<br>
@@ -67,13 +66,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg12 text-center">
-                    <h1 class="mb-4">充電器の種類</h1>
-                    <p class="title-border mb-5"></p>
+                    <h1 class="mb-4 sec-title">充電器の種類</h1>
                 </div>
             </div>
         </div>
 
-        <div class="container">
+        <div class="container mt-5">
             <div class="multiple-items text-center">
                 <div>
                     <a href=""><img class="img-thumbnail" src="{{ asset('img/USB-PD.png') }}"></a>
