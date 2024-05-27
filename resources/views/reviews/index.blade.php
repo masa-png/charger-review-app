691e0d8f316a54ded7a85529dee6dc9b19b3ca42
masa-png <adidas.org@icloud.com>
2024-05-27 12:27:08 +0900

    編集

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -35,17 +35,13 @@
                     </button>
                 </div>
 
-                {{-- <div>
-                    <img src="{{ asset('img/IMG_0835.jpeg') }}" class="img-thumbnail">
-                </div> --}}
-
                 <div class="card mt-5" style="width: 26rem;">
                     <div class="card-body">
                         <h2 class="card-title">{{ $review->product->vendor->name }}</h2>
                         <h3 class="card-subtitle mb-2 text-body-secondary">{{ $review->product->name }}</h3>
-                        <h4 class="card-text mb-3">種類:{{ $review->product->type->name }}タイプ</h4>
-                        <h4 class="card-text mb-3">W数:{{ $review->product->wattage->watt }}W</h4>
-                        <h4 class="card-text mb-3">値段:{{ $review->product->price }}</h4>
+                        <h4 class="card-text mb-3">種類 {{ $review->product->type->name }}タイプ</h4>
+                        <h4 class="card-text mb-3">W数 {{ $review->product->wattage->watt }}W</h4>
+                        <h4 class="card-text mb-3">¥{{ number_format($review->product->price) }}</h4>
                         <h4 class="card-text mb-3">商品のリンク先:<a
                                 href="https://www.amazon.co.jp/NovaPort-USB-C-%E3%80%90CIO%E7%8B%AC%E8%87%AA%E6%8A%80%E8%A1%93-NovaIntelligence%E6%90%AD%E8%BC%89%E3%80%91-CIO-G45W2C/dp/B0BHCH1BF9/ref=sr_1_8?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=2TDLCH5D6QEJG&dib=eyJ2IjoiMSJ9.wFeJS29_wZ9syhfdQw7XflWAPjQ19mHo1XswNUgqhSRdXO7reQVxlcr63Hvl2DgWiZyVGJxYC2wowwA4DHOkcZt7b15aevwoWBWgE0sdNVlBy2mxFBmxziFkCSEV5G7sNOStRdnZCIP_ucZlui5181TDDpUb2f9XA1NxBZ2Jq2wbMeaCAoxqFLEmio5NANpiutYb1r6LvX2t17rEBD6aZNtuhrINCzM2m8Z92tMXzunrWBJUolCbayjvoeA11ULCOH6Pu34ZOZ8IQB_5A7J8QQfaWbNXTbBurEeMgPyVpNw.uXS4mrKRCIeJHxNk9xZLkeTVsgI8ndqhroTu92gJQuY&dib_tag=se&keywords=cio&qid=1716107379&sprefix=cio%2Caps%2C164&sr=8-8&th=1">
                                 link
@@ -58,8 +54,8 @@
             <div class="col-md-6">
                 <div class="mb-5">
                     <h3 class="fw-bold">{{ $review->title }}</h3>
-                    <p class="fs-5 mb-2"><span class="review-score-color">{{ $review->score }}★★★★★</span><span
-                            class="review-score-blank-color"></span></p>
+                    <p class="fs-5 mb-2"><span class="review-score-color">{{ str_repeat('★', $review->score) }}</span><span
+                            class="review-score-blank-color">{{ str_repeat('★', 5 - $review->score) }}</span></p>
                     <p>{{ $review->content }}</p>
                     <p><span class="fw-bold me-2">{{ $review->user->name }}</span><span
                             class="text-muted">{{ $review->created_at }}</span></p>
@@ -82,8 +78,8 @@
                 </div>
 
                 <div class="mt-5">
-                    <div>
-                        <div class="comment-body">
+                    <div class="comment-body">
+                        <div class="comment-content">
                             <h5 class="mt-0">
                                 <a href="#">ユーザー名</a>
                                 <a href="#">
@@ -91,19 +87,19 @@
                                 </a>
                             </h5>
                             テストコメント
-                            <div class="form-group mt-3">
-                                <textarea name="content" class="form-control" rows="1" placeholder="返信を追加..."></textarea>
-                                <div class="mt-3 text-end">
-                                    <a class="btn btn-outline-secondary me-3" href="#" role="button">キャンセル</a>
-                                    <a class="btn submit-button text-white" href="#" role="button">コメント</a>
-                                </div>
+                        </div>
+                        <div class="form-group mt-3">
+                            <textarea name="content" class="form-control" rows="1" placeholder="返信を追加..."></textarea>
+                            <div class="mt-3 text-end">
+                                <a class="btn btn-outline-secondary me-3" href="#" role="button">キャンセル</a>
+                                <a class="btn submit-button text-white" href="#" role="button">コメント</a>
                             </div>
+                        </div>
 
-                            <div>
-                                <h5 class="mt-3">
-                                    <a href="#">~件の返信</a>
-                                </h5>
-                            </div>
+                        <div>
+                            <h5 class="mt-3">
+                                <a href="#">~件の返信</a>
+                            </h5>
                         </div>
                     </div>
                 </div>
