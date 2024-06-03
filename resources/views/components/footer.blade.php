a2eca02f73fe531034b9ab1fa2118a4b510ccb30
masa-png <adidas.org@icloud.com>
2024-06-03 19:13:11 +0900

    footerを追加

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="d-flex justify-content-center align-items-center mt-5">
+    <a href="{{ url('/') }}" class="text-center text-white">
+        &copy;{{ config('app.name', 'Laravel') }}
+    </a>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 
 <body>
-    <div id="app">
+    <div id="app" class="wrapper">
         @component('components.header')
         @endcomponent
 
@@ -31,10 +31,9 @@
             @yield('content')
         </main>
 
-        {{-- @component('components.footer')
-        @endcomponent --}}
+        @component('components.footer')
+        @endcomponent
     </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
