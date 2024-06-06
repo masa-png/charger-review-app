697a852ca2999201fa5097567c183c36a7507e96
masa-png <adidas.org@icloud.com>
2024-06-06 15:26:20 +0900

    imageのバリデーションを追記

--- app/Http/Requests/ReviewRequest.php
+++ app/Http/Requests/ReviewRequest.php
@@ -27,6 +27,7 @@ class ReviewRequest extends FormRequest
             'type_id' => 'required',
             'wattage_id' => 'required',
             'price' => 'required|integer',
+            'image' => 'file|image|multiple_of:3|mimes:png,jpg,jpeg',
             'score' => 'required|numeric',
             'title' => 'required|string|max:50',
             'content' => 'required|string|max:200'
