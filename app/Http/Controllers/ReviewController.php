abb9b43bdac16c250858866f4ab9d03051d47458
masa-png <adidas.org@icloud.com>
2024-06-15 13:20:50 +0900

    メソッドの移動

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProductController;
 use App\Http\Requests\ReviewRequest;
+use Illuminate\Support\Facades\Storage;
 
 class ReviewController extends Controller
 {
@@ -26,19 +27,27 @@ class ReviewController extends Controller
     //  */
     public function store(ReviewRequest $request)
     {
+        $product = new Product();
+
+        // s3に保存
+        $path = Storage::disk('s3')->putFile('', $request->file('image'));
+
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->vendor_id = $request->input('vendor_id');
+        $product->wattage_id = $request->input('wattage_id');
+        $product->type_id = $request->input('type_id');
+        $product->image_path = Storage::disk('s3')->url($path);
+        $product->save();
+
         $review = new Review();
-
-        $product_count = Product::all()->count();
-
-        // ProductControllerのstore()を実行
-        ProductController::store($request);
 
         $review->title = $request->input('title');
         $review->content = $request->input('content');
         $review->score = $request->input('score');
-        $review->product_id = $product_count + 1;
         $review->user_id = Auth::id();
-        $review->save();
+
+        $product->reviews()->save($review);
 
         return to_route('mypage')->with('flash_message', 'レビュー記事を投稿しました。');
     }
@@ -50,8 +59,18 @@ class ReviewController extends Controller
     {
         $product = Product::find($review->product_id);
 
-        // ProductControllerのstore()を実行
-        ProductController::update($request, $product);
+        if ($request->file('image') !== null) {
+            // s3に保存
+            $path = Storage::disk('s3')->putFile('', $request->file('image'));
+            $product->image_path = Storage::disk('s3')->url($path);
+        }
+
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->vendor_id = $request->input('vendor_id');
+        $product->wattage_id = $request->input('wattage_id');
+        $product->type_id = $request->input('type_id');
+        $product->update();
 
         $review->title = $request->input('title');
         $review->content = $request->input('content');
