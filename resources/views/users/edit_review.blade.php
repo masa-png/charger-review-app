7f36e92b0f3995e6908bdcd1f821ab6842b76ac2
masa-png <adidas.org@icloud.com>
2024-06-15 10:16:33 +0900

    間隔の調整

--- resources/views/users/edit_review.blade.php
+++ resources/views/users/edit_review.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container pt-5">
+    <div class="container py-4">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
