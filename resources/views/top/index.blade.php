585f352c26ecccd5b9542b3546a0a4ade7c9cc2e
masa-png <adidas.org@icloud.com>
2024-06-21 14:51:19 +0900

    修正

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -33,7 +33,7 @@
                     <h1 class="mb-4 sec-title">新着記事</h1>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-3 g-5 mb-4">
+            <div class="row row-cols-1 row-cols-md-3 g-4 mb-4">
                 @foreach ($recently_reviews as $recently_review)
                     <div class="col">
                         <div class="card h-100">
